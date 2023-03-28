0d256290cd637e392c3f14ab09b9663bff330c81
KelleD01 <KelleD01@BC84372>
2023-03-28 15:57:46 +0100

    Orders

---
+++ Orders_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("orders_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("orders_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("4C81DEF3-7725-46BA-AD6A-E2CFD4C555EF")]
+[assembly: AssemblyVersion("1.0.0.0")]
